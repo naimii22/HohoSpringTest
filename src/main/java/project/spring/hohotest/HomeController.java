212d084570b17480bab7b0db12f1e9a9c2d86409
bhr0554 <bhr5004@gmail.com>
2018-12-28 00:13:15 +0900

    혜련 HomeController 수정

--- src/main/java/project/spring/hohotest/HomeController.java
+++ src/main/java/project/spring/hohotest/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("수정되겠지?");
 		return "home";
 	}
 	
