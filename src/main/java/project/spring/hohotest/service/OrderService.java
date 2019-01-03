8b8b792bb6aede07756b45a21056870ea0875253
naim <pinktor157@gmail.com>
2019-01-04 00:23:14 +0900

    나임_orderList 회원ID 가져오기

--- src/main/java/project/spring/hohotest/service/OrderService.java
+++ src/main/java/project/spring/hohotest/service/OrderService.java
@@ -7,8 +7,9 @@ import project.spring.hohotest.model.Orders;
 /** 주문 관련 기능을 제공하기 위한 Service 계층 */
 public interface OrderService {
 	
-	public void insertOrder(Orders order) throws Exception;				// 주문 추가
-	public Orders selectOrder(Orders order) throws Exception;				// 하나의 주문보기
-	public List<Orders> selectOrderList(Orders order) throws Exception;	// 주문 목록 조회
-	public int selectOrderCount(Orders order) throws Exception;			// 전체 주문 수 조회
+	public void insertOrder(Orders order) throws Exception;									// 주문 추가
+	public Orders selectOrder(Orders order) throws Exception;								// 하나의 주문보기
+	public List<Orders> selectOrderList(Orders order) throws Exception;						// 주문 목록 조회
+	public int selectOrderCount(Orders order) throws Exception;								// 전체 주문 수 조회
+	public String selectUserId(Orders order) throws Exception;			// 회원 아이디 조회
 }
